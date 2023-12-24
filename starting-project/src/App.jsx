fabe7027f24880a7d0def6d6b4fa998bef89f50f
Progress with the component <deMiguel@newsaktuell.de>
2023-12-24 10:21:47 +0100

    refactor to apply header step 1

--- starting-project/src/App.jsx
+++ starting-project/src/App.jsx
@@ -1,47 +1,10 @@
-import { useState } from "react";
-import classes from "./App.module.css";
-import Post from "./components/Post/Post";
-import Modal from "./components/modal/Modal";
-import NewPost from "./components/newPost/NewPost";
 import PostList from "./components/postList/PostList";
 
-const cards = [
-  { author: "John", message: "Hello" },
-  { author: "Bob", message: "Hi" },
-];
-
 function App() {
-  const [modalIsVisible, setModalIsVisible] = useState(true);
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  const hideModalHandler = () => {
-    setModalIsVisible(false);
-  };
-
-  const bodyChangeHandler = (event) => {
-    setEnteredBody(event.target.value);
-  };
-
-  const authorChangeHandler = (event) => {
-    setEnteredAuthor(event.target.value);
-  };
-
   return (
-    <>
-      {modalIsVisible ? (
-        <Modal onClose={hideModalHandler}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-          />
-        </Modal>
-      ) : null}
-      <ul className={classes.post}>
-        <Post author={enteredAuthor} message={enteredBody} />
-        <PostList cards={cards} />
-      </ul>
-    </>
+    <main>
+      <PostList />
+    </main>
   );
 }
 
--- starting-project/src/components/postList/PostList.jsx
+++ starting-project/src/components/postList/PostList.jsx
@@ -1,11 +1,46 @@
+import { useState } from "react";
 import Post from "../Post/Post";
+import Modal from "../modal/Modal";
+import NewPost from "../newPost/NewPost";
+import classes from "./PostList.module.css";
 
-export default function PostList(props) {
+const cards = [
+  { author: "John", message: "Hello" },
+  { author: "Bob", message: "Hi" },
+];
+
+export default function PostList() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  const hideModalHandler = () => {
+    setModalIsVisible(false);
+  };
+
+  const bodyChangeHandler = (event) => {
+    setEnteredBody(event.target.value);
+  };
+
+  const authorChangeHandler = (event) => {
+    setEnteredAuthor(event.target.value);
+  };
   return (
     <>
-      {props.cards.map((card) => (
-        <Post key={card.author} author={card.author} message={card.message} />
-      ))}
+      {modalIsVisible ? (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      ) : null}
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} message={enteredBody} />
+        {cards.map((card) => (
+          <Post key={card.author} author={card.author} message={card.message} />
+        ))}
+      </ul>
     </>
   );
 }
